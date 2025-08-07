3092578309d9834e93de9ab21a66060d69ec5998
nic <nickjemorris4@gmail.com>
2025-08-07 23:38:21 +0100

    remove existing map_service tests

--- tests/api/services/test_map_service.py
+++ tests/api/services/test_map_service.py
@@ -3,13 +3,15 @@ Map Service Unit Tests.
 """
 
 import random
+from datetime import date
 
 import pytest
 
-from src.api.constants import ModHubMapFilters
+from src.api.constants import ModHubMapFilters, FarmhandMapFilters
 from src.api.core.db.models import Map
 from src.api.core.repositories import MapRepository
-from src.api.core.schema.mods import ModModel
+from src.api.core.schema.maps import MapModel
+from src.api.core.schema.mods import ModDetailModel
 from src.api.services.map_service import MapService
 from src.api.services.modhub_service import ModHubService
 
@@ -23,7 +25,7 @@ class TestMapService:
         Fixture specifying the return mod_detail object from the
         mocked mod_hub_service.
         """
-        return ModModel(
+        return ModDetailModel(
             id=123456,
             name="Custom Map 1",
             Game="FS25",
@@ -41,9 +43,9 @@ class TestMapService:
     @pytest.fixture
     async def mock_mod_hub_service(self, mocker, mod_detail):
         """
-        Mock the modhub service calls used within the map service with a
+        Mock the ModHUb service calls used within the map service with a
         single id and mod model.
-        :param mocker: pytest mocker fixture
+        :param mocker: Pytest mocker fixture
         :param mod_detail: the mocked mod detail to return
         """
 
@@ -56,86 +58,79 @@ class TestMapService:
         mocker.patch.object(ModHubService, "get_pages", return_value=[0])
         mocker.patch.object(ModHubService, "scrape_mod", return_value=mod_detail)
 
-    async def test_map_service_scrapes_maps(self, db, mock_mod_hub_service, mod_detail):
+    def test_get_all_maps(self, db):
         """
-        Test that the map_service creates a map from the mock mod hub service fixture.
-        :param mock_mod_hub_service: mock modhub service fixture
-        :param mod_detail: mod detail fixture
-        """
-
-        map_service = MapService(db)
-        map_repository = MapRepository(db)
-        await map_service.scrape_maps()
-
-        assert len(map_repository.all()) == 1
-
-        expected_map: Map = map_repository.get_by_id(mod_detail.id)
-
-        assert expected_map.id == mod_detail.id
-        assert expected_map.name == mod_detail.name
-        assert expected_map.author == mod_detail.author
-        assert expected_map.category == "map_europe"
-        assert expected_map.release_date == str(mod_detail.release_date)
-        assert expected_map.version == mod_detail.version
-        assert expected_map.zip_filename == mod_detail.zip_filename
-
-    async def test_get_map_updates_based_on_mod_version(self, db, mock_mod_hub_service, mod_detail):
-        """
-        Test that the map service updates a map based on its version when scraping maps
-        from modhub.
-        :param mock_mod_hub_service: mock modhub service fixture
-        :param mod_detail: mod detail fixture
+        Test that the map service can retrieve all map data from the
+        repository.
+        :param db: Database Session fixture.
         """
         map_repository = MapRepository(db)
+
         map_repository.create(
-            id=654321,
-            name="Calmsden Farms",
+            id=1,
+            name="Custom Farms",
             category="European Maps",
             author="user",
             release_date="30-04-2025",
             version="1.0.0.0",
         )
 
-        mod_detail.id = 654321
-        mod_detail.name = "Oak Bridge Farm"
-        mod_detail.version = "1.1.0.0"
+        map_repository.create(
+            id=2,
+            name="Custom Farms 2",
+            category="European Maps",
+            author="user2",
+            release_date="07-08-2025",
+            version="1.0.3.0",
+        )
 
         map_service = MapService(db)
-        await map_service.scrape_maps()
+        assert len(map_service.get_maps()) == 2
 
-        expected_map: Map = map_repository.get_by_id(mod_detail.id)
-
-        # assert the mod_detail version and mod detail name are not equal.
-        assert mod_detail.version != expected_map.version
-        assert mod_detail.name != expected_map.name
-
-    async def test_get_map_does_not_update_for_same_mod_version(
-        self, db, mock_mod_hub_service, mod_detail
-    ):
+    def test_get_map_by_id(self, db):
         """
-        Test that when getting maps from modhub, no update is applied if the
-        version number is the same.
-        :param mock_mod_hub_service: mock modhub service fixture
-        :param mod_detail: mod detail fixture
+        Test that the map service can get a saved map by its
+        mod / map_id.
+        :param db: Database Session Fixture.
         """
+        map_id: int = 123456
         map_repository = MapRepository(db)
-        map_id = random.randint(100000, 999999)
 
         map_repository.create(
             id=map_id,
-            name="Custom Map 1",
-            category="map-europe",
+            name="Custom Farms",
+            category="European Maps",
             author="user",
             release_date="30-04-2025",
             version="1.0.0.0",
         )
 
-        mod_detail.id = map_id
+        map_service = MapService(db)
+        assert map_service.get_map_by_id(map_id) is not None
+
+    def test_create_map(self, db):
+        """
+        Test that the map service can create a new map and
+        save it in the database.
+        :param db: Database Session Fixture.
+        """
+        map_id: int = 999999
+        new_map = MapModel(
+            id=map_id,
+            name="New Map Name",
+            category=FarmhandMapFilters.EUROPEAN_MAPS,
+            author="Nicholas Angel",
+            release_date=date(day=7, month=8, year=2025),
+            version="1.0.1.0",
+            zip_filename="new_map_name.zip"
+        )
 
         map_service = MapService(db)
-        await map_service.scrape_maps()
+        map_service.create_map(new_map)
+        assert map_service.get_map_by_id(map_id) is not None
 
-        expected_map: Map = map_repository.get_by_id(mod_detail.id)
+    def test_get_new_maps(self):
+        pass
 
-        assert mod_detail.version == expected_map.version
-        assert mod_detail.name == expected_map.name
+    def test_get_new_maps_with_no_new_maps(self):
+        pass
