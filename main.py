1093b4858290dd66c4c36ebb3fe692f15756d5d3
nic <nickjemorris4@gmail.com>
2026-07-21 20:28:34 +0100

    fix: update default error handler response code

--- main.py
+++ main.py
@@ -54,5 +54,5 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
     """
     error = format_pydantic_errors(exc)
     return JSONResponse(
-        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, content=jsonable_encoder(error)
+        status_code=status.HTTP_422_UNPROCESSABLE_CONTENT, content=jsonable_encoder(error)
     )
